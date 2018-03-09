597c758257077d1e8926c7fe678bcfe0b2f764ba
winand appels <winnie33winnie33@gmail.com>
2018-03-09 20:01:48 +0100

    5% chance, what am I doing to this world...

--- rollCena.js
+++ rollCena.js
@@ -1,17 +1,19 @@
 // The world needs this.
 
 $(document).ready(function() {
-  if (Math.random() < 1) {
+  if (Math.random() < 0.05) {
     itsJohnCena();
   }
 });
 
 itsJohnCena = function() {
+  document.getElementsByTagName("body")[0].innerHTML = "";
   var vid = document.createElement("video");
   var source = document.createElement("source");
   vid.width = 1280 // $(window).width();
   vid.height = 720 // $(window).height();
   vid.autoplay = true; // Obviously
+  vid.loop = true; // Nobody expects the second John Cena!
 
   // We tell the source object where to get the video from.
   source.src = "https://github.com/winnie334/unexpectedCena/raw/master/bestVidEver.mp4";
