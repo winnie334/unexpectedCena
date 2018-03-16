11cf8c78b5faa0c772752c66d0d47df5bde745bd
Winnie33 <winnie33winnie33@gmail.com>
2018-03-16 19:39:02 +0100

    Update to support webm as well

--- rollCena.js
+++ rollCena.js
@@ -9,17 +9,22 @@ $(document).ready(function() {
 itsJohnCena = function() {
   document.getElementsByTagName("body")[0].innerHTML = "";
   var vid = document.createElement("video");
-  var source = document.createElement("source");
+  var source1 = document.createElement("source");
+  var source2 = document.createElement("source");
   vid.width = 1280 // $(window).width();
   vid.height = 720 // $(window).height();
   vid.autoplay = true; // Obviously
   vid.loop = true; // Nobody expects the second John Cena!
+  vid.innerHTML = "You don't support the cena :(";
 
   // We tell the source object where to get the video from.
-  source.src = "https://github.com/winnie334/unexpectedCena/raw/master/bestVidEver.mp4";
-  source.type = "video/mp4";
+  source1.src = "https://github.com/winnie334/unexpectedCena/raw/master/bestVidEver.mp4";
+  source1.type = "video/mp4";
+  source2.src = "https://github.com/winnie334/unexpectedCena/raw/master/bestVidEver.webm";
+  source2.type = "video/webm";
 
   // We add the video source to the video object.
-  vid.appendChild(source);
+  vid.appendChild(source1);
+  vid.appendChild(source2);
   document.getElementsByTagName("body")[0].appendChild(vid);
 }
